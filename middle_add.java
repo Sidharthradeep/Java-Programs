9a820a544666c6aafd66eb9ba1f9279e39e81290
Sidharth Radeep M <116501635+Sidharthradeep@users.noreply.github.com>
2025-01-17 11:26:56 +0530

    Word adding in middle of a sentence

---
+++ middle_add.java
@@ -0,0 +1,12 @@
+package controlstatements;
+
+public class middle_add {
+
+	public static void main(String[] args) {
+		String str="Java 3.0";
+		String s="Tutorial";
+		String res=str.substring(0,5)+s+str.substring(4);
+		System.out.println(res);
+	}
+
+}
