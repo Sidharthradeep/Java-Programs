d2a27661915a2e87c87b05f9fa9fad382b385cfd
Sidharth Radeep M <116501635+Sidharthradeep@users.noreply.github.com>
2025-01-17 11:26:04 +0530

    Odd number print in java

---
+++ oddnumber_print.java
@@ -0,0 +1,16 @@
+package controlstatements;
+
+public class oddnumber_print {
+
+	public static void main(String[] args) {
+		for(int i=0;i<100;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
