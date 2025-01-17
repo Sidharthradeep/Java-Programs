606e256d42a94edbf32f9b500957705aa537e637
Sidharth Radeep M <116501635+Sidharthradeep@users.noreply.github.com>
2025-01-17 12:04:48 +0530

    Replace String

---
+++ replace_string.java
@@ -0,0 +1,12 @@
+package controlstatements;
+
+public class replace_string {
+
+	public static void main(String[] args) {
+		String str="Hello World";
+		String s=str.replace("Hello","Hi");
+		System.out.println(s);
+
+	}
+
+}
