12b1fae6757a68791ae47a7a982cb0bc31c2b8c8
Sidharth Radeep M <116501635+Sidharthradeep@users.noreply.github.com>
2025-01-17 12:05:24 +0530

    Count of Numbers

---
+++ onetoten.java
@@ -0,0 +1,16 @@
+package controlstatements;
+
+public class onetoten {
+
+	public static void main(String[] args) {
+		int dig=2345;
+		int count=0;
+		while(dig!=0)
+		{
+			dig=dig/10;
+			count++;
+		}
+		System.out.println(count);
+		
+	}
+}
